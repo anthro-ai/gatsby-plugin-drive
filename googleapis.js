8e5c9de984a9acdfe2f805ac6c7b6f391385370b
Yin Shanyang <me@yinshanyang.com>
2019-04-05 16:58:58 +0530

    Make the git repo module-like

---
+++ googleapis.js
@@ -0,0 +1,89 @@
+const { GoogleToken } = require('gtoken');
+const request = require('request');
+
+const getToken = keyFile => {
+  return new Promise((resolve, reject) => {
+    const gtoken = new GoogleToken({
+      keyFile,
+      scope: ['https://www.googleapis.com/auth/drive']
+    });
+
+    gtoken.getToken((err, token) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(token);
+      }
+    });
+  });
+};
+
+const getFolder = (folderId, token) => {
+  return new Promise((resolve, reject) => {
+    request({
+      uri: `https://www.googleapis.com/drive/v3/files`,
+      auth: {
+        bearer: token
+      },
+      qs: {
+        fields: 'kind,files(kind,id,name,mimeType,modifiedTime,createdTime)',
+        q: `'${folderId}' in parents`
+      }
+    }, (err, res, body) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(JSON.parse(body).files);
+      }
+    });
+  });
+};
+
+const getFile = (fileId, token) => {
+  return new Promise((resolve, reject) => {
+    request({
+      uri: `https://www.googleapis.com/drive/v3/files/${fileId}`,
+      auth: {
+        bearer: token
+      },
+      encoding: null,
+      qs: {
+        alt: 'media'
+      }
+    }, (err, res, body) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(body);
+      }
+    });
+  });
+};
+
+const getGDoc = (fileId, token, mimeType) => {
+  return new Promise((resolve, reject) => {
+    request({
+      uri: `https://www.googleapis.com/drive/v3/files/${fileId}/export`,
+      auth: {
+        bearer: token
+      },
+      encoding: null,
+      qs: {
+        mimeType: mimeType
+      }
+    }, (err, res, body) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(body);
+      }
+    });
+  });
+};
+
+module.exports = {
+  getToken,
+  getFolder,
+  getFile,
+  getGDoc
+};
